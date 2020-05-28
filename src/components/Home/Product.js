54562b5afa29d39ececd83d7e0d821e10837dcd6
Jeffrey Walsh <marwal@Jeffreys-iMac-Pro-4.local>
2020-05-28 10:15:31 -0700

    updated css

--- src/components/Home/Product.js
+++ src/components/Home/Product.js
@@ -3,12 +3,12 @@ import Img from 'gatsby-image';
 
 export default function Product({product}) {
     return (
-        <div className="col-10 col-sm col-md-6 col-lg-4 mx-auto my-3">
+        <div className="col-4 col-sm-4 col-md-4 col-lg-4  mx-auto my-3">
             <div className="card" style={{minHeight:"100%"}}>
                 <Img fluid={product.image.fluid} className="card-img-top" />
                 <div className="card-body text-center">
-                <h6>{product.title}</h6>
-                <h6>{product.price}</h6>
+                <h6 className="producttext">{product.title}</h6>
+                <h6 className="productprice">{product.price}</h6>
                 <button className="btn btn-yellow mt-3 text-capitalize snipcart-add-item"
                 data-item-id={product.id}
                 data-item-name={product.title}
