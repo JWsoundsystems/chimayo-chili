7eff198cfc270bb90f0a6517fe12c67a846222f3
Jeffrey Walsh <marwal@Jeffreys-iMac-Pro-4.local>
2020-05-29 18:19:32 -0700

    final css touches to responsive website

--- src/components/Home/Info.js
+++ src/components/Home/Info.js
@@ -8,15 +8,21 @@ export default function Info() {
             <div className="container infos">
             <Title title="About Us"></Title>
             <div className="row">
-            <div className="col-10 col-sm-8 mx-auto text-center">
+            <div className="col-12 col-sm-12 col-md-12 mx-auto text-center">
                 <p className="lead text-black mb-5 mx-auto font-size-sml">
                     Cupidatat eu aute occaecat labore commodo elit nulla magna. Non aliquip commodo anim exercitation ad elit dolor. Sunt aliquip pariatur laboris do aliqua commodo tempor proident nisi elit.
                     Cupidatat eu aute occaecat labore commodo elit nulla magna. Non aliquip commodo anim exercitation ad elit dolor. Sunt aliquip pariatur laboris do aliqua commodo tempor proident nisi elit. 
+                    <br></br>
+                    <br></br>
                     Cupidatat eu aute occaecat labore commodo elit nulla magna. Non aliquip commodo anim exercitation ad elit dolor. Sunt aliquip pariatur laboris do aliqua commodo tempor proident nisi elit.
-                    Cupidatat eu aute occaecat labore commodo elit nulla magna. Non aliquip commodo anim exercitation ad elit dolor. Sunt aliquip pariatur laboris do aliqua commodo tempor proident nisi elit. 
+                    Cupidatat eu aute occaecat labore commodo elit nulla magna. Non aliquip commodo anim exercitation ad elit dolor. Sunt aliquip pariatur laboris do aliqua commodo tempor proident nisi elit.
+                    <br></br>
+                    <br></br>
+                    Cupidatat eu aute occaecat labore commodo elit nulla magna. Non aliquip commodo anim exercitation ad elit dolor.
+                      
                 </p>
                 <Link to="/about/">
-                    <button className="btn text-uppercase btn-yellow">About Page</button>
+                    <button className="btn text-uppercase btn-yellow btnAbout">About Page</button>
                 </Link>
 
             </div>
